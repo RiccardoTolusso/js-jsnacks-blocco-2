165be0e27d0f111276c031aefa707223bfcf8647
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-08 17:30:46 +0200

    [ADD] file structure for snack-d

---
+++ snack-d/js/main.js
@@ -0,0 +1,14 @@
+// importo riccardosLength dallo snack-a
+
+// definisco una funzione clonePieceOfArray(inputArray, start, stop)
+
+//     creo una costante result e la inizializzo ad array vuoto []
+
+//     creo un ciclo for per valori di i che partono da start e che siano minori di stop con step di 1
+
+//         aggiungo a result in posizione i-start il valore di inputArray in posizione i
+    
+//     restituisco l'array contenuto in result
+
+// definisco una funzione rimuoviDallaCoda(inputArray)
+//     invoco e restituisco la funzione clonePieceOfArray(inputArray, 0, riccardosLength(inputArray)-1)
