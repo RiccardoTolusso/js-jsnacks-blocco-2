536f799266ed206157ec7c3c08bbe148f7469d36
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-08 15:16:17 +0200

    [UPD] END OF EXERCISE

--- snack-a/js/main.js
+++ snack-a/js/main.js
@@ -1,1 +1,18 @@
 "use strict"
+
+// definisco un funzione riccardosLength() che abbia come parametro un array chiamato inputArray
+function riccardosLength(inputArray){
+    // creo una variabile risultato e la pongo = 0
+    let result = 0;
+    // fintanto che il valore dell'inputArray nella posizione risultato sia diverso da undefined
+    while (inputArray.at(result) !== undefined){
+        // incremento la variabile risultato di 1
+        result += 1
+    }
+    // restituisco il valore di result
+    console.log(result)
+}
+
+riccardosLength([])
+
+
